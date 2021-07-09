a234a75d5c8e044588a2a0728cd8ffa7effab173
hy2000-1111 <2284311156@qq.com>
2021-07-09 13:24:02 +0800

    弹窗

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,105 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleBtnClick() {
+    console.log("handleBtnClick");
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handleShowToast() {
+    wx.showToast({
+      title: '弹窗标题',
+      // 弹窗时间
+      duration: 3000,
+      // success error loading none
+      icon: "success",
+      // 必须是本地图片,优先级高于icon
+      image: '/image/img/miaomi.jpg',
+      // 是否显示透明蒙层，防止触摸穿透(弹窗出来的时候不监听其他按钮点击)
+      mask: true,
+      success() {
+        console.log("success");
+      },
+      fail() {
+        console.log("fail");
+      },
+      // 接口调用结束的回调函数
+      complete() {
+        console.log("complete");
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  handleShowModal() {
+    wx.showModal({
+      title: '标题',
+      content: '内容',
+      // 是否显示取消按钮
+      showCancel: true,
+      // 取消按钮的文字
+      cancelText: '取消吧',
+      // 取消按钮的文字颜色
+      cancelColor: '#0f0',
+      // 确认按钮的文字
+      confirmText: "确认吧",
+      // 确认按钮的文字颜色
+      confirmColor: '#858',
+      success(event) {
+        console.log("success",event.cancel);
+      },
+      fail() {
+        console.log("fail");
+      },
+      complete() {
+        console.log("complete");
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  handleShowLoading() {
+    wx.showLoading({
+      title: 'loading',
+      mask: true,
+      success() {
+        console.log("showLoading success");
+      },
+      fail() {
+        console.log("showLoading fail");
+      },
+      complete() {
+        console.log("showLoading complete");
+        setTimeout(() => {
+          wx.hideLoading({
+            success: (res) => {
+              console.log("hideLoading success");
+            },
+          })
+        },2000)
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  handleShowActionSheet() {
+    wx.showActionSheet({
+      alertText: "警示文案",
+      itemList: ["一","二","三"],
+      itemColor: "#0f0",
+      success() {
+        console.log("showActionSheet success");
+      },
+      fail() {
+        console.log("showActionSheet fail");
+      },
+      complete() {
+        console.log("showActionSheet complete");
+      }
+    })
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onShareAppMessage(options) {
+    return {
+      // 转发时小程序的标题，不设置默认以项目名组为小程序名
+      title: "hy miniprogram",
+      // 分享出去的小程序点击后进入的页面，不设置默认进入小程序首页
+      path: "/pages/about/about.js",
+      // 分享出去的小程序显示的图片，不设置默认以截图首页作为图片
+      imageUrl: '/image/img/miaomi.jpg'
+    }
   }
 })
