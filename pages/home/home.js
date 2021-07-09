e402667f4753c77609a98b5cb7692db1977b7d26
hy2000-1111 <2284311156@qq.com>
2021-07-09 11:22:09 +0800

    网络请求

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,69 @@
-// pages/home/home.js
+import request from "../../service/network"
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
+    // 使用原生的方式发送网络请求
+    this.get_data_origin();
 
+    // 使用封装的方式发送网络请求
+    request({
+      url: "http://152.136.185.210:7878/api/m5/recommend"
+    }).then(res => {
+      console.log(res);
+    }).catch(err => {
+      console.log(res);
+    })
   },
+  get_data_origin() {
+        // get请求
+    // wx.request({
+    //   url: "http://152.136.185.210:7878/api/m5/recommend",
+    //   success:(res) => {
+    //     console.log(res);
+    //   }
+    // })
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    // get请求
+    // wx.request({
+    //   url: 'http://152.136.185.210:7878/api/m5/home/data?type=sell&page=1',
+    //   success(res){
+    //     console.log(res);
+    //   }
+    // })
 
-  },
+    // 携带参数的get请求
+    // wx.request({
+    //   url: 'http://152.136.185.210:7878/api/m5/home/data',
+    //   data: {
+    //     type: 'sell',
+    //     page: 1 
+    //   },
+    //   success(res){
+    //     console.log(res);
+    //   }
+    // })
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+    // post请求携带参数
+    // wx.request({
+    //   url: 'http://httpbin.org/post',
+    //   method: "post",
+    //   data: {
+    //     name: 'hy',
+    //     age: 18
+    //   },
+    //   success(res){
+    //     console.log(res);
+    //   },
+    //   // 请求失败的回调
+    //   fail(err) {
+    //     console.log(err);
+    //   },
+    //   // 接口调用结束后的回调函数 成功失败都会执行
+    //   complete() {
 
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    //   }
+    // })
   }
 })
